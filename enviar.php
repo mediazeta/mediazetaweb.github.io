2fd22d0d1f2609713673ef32f638ce6b532ef268
alvaro <alvarobustamantecorominas@gmail.com>
2023-04-28 00:12:59 +0200

    formulario

---
+++ enviar.php
@@ -0,0 +1,15 @@
+<?php
+if(isset($_POST['submit'])) {
+    $to = "mediazetainfo@gmail.com"; // Cambiar por la dirección de correo electrónico a la que deseas enviar el mensaje
+    $subject = "Mensaje enviado desde el formulario de contacto";
+    $name = $_POST['name'];
+    $apellidos = $_POST['apellidos'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $headers = "From: $email";
+
+    mail($to, $subject, "Nombre: ".$name."\nApellidos: ".$apellidos."\nEmail: ".$email."\n\n".$message, $headers);
+}
+?>
+
+
